6c6b29f2c14a612374015029782abbd25d4c9b37
devsuin <suin24680@gmail.com>
2025-12-28 03:24:27 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 const express = require('express')
 const app = express()
 const port = 4000;
+const { User } = require("./models/User");
+const config = require('./config/key');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const mongoose = require("mongoose")
-mongoose.connect('mongodb+srv://maeum:suin672033@maeum.anygtuo.mongodb.net/?appName=Maeum')
+mongoose.connect(config.mongoURI)
 .then(() => console.log('MongoDB Connected. . .'))
 .catch((err) => console.log(err))
 
-const meRouter = require("./routes/me");
 
-app.use("/api", meRouter);
+app.get('/', (req, res) => res.send('Hello World happy'))
 
-app.get('/', (req, res) => res.send('Hello World'))
+app.post('/register', async (req,res) => {
+    //회원 가입할 때 필요한 정보들을 client에서 가져오면
+    //그것들을 데이터 베이스에 넣어준다.
+    try {
+        const user = new User(req.body);
+        const savedUser = await user.save()     // 콜백X
+        return res.status(200).json({ success: true });
+    } catch (err) {
+        console.log(err);
+        return res.status(400).json({success: false, err});
+    }
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
